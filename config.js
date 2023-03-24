75780258d367b0e830776d772676c03e65019565
Luscas123 <102263815+Luscas123@users.noreply.github.com>
2023-03-24 20:27:00 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 require("@firebase/firestore");
 
-var firebaseConfig = {
-  apiKey: "AIzaSyCZrG0BF6LrnRpWZjsPQxNGyMCLByo8HrM",
-  authDomain: "e-ride-549e4.firebaseapp.com",
-  projectId: "e-ride-549e4",
-  storageBucket: "e-ride-549e4.appspot.com",
-  messagingSenderId: "588298110700",
-  appId: "1:588298110700:web:34e3df331c6b1fc95047c9",
-  measurementId: "G-G3K7BWL5LH"
+const firebaseConfig = {
+  apiKey: "AIzaSyA8OHpEdHkKZaXMRIxo-lwh08O93nEsq5k",
+  authDomain: "ciclista-eletroni.firebaseapp.com",
+  databaseURL: "https://ciclista-eletroni-default-rtdb.firebaseio.com",
+  projectId: "ciclista-eletroni",
+  storageBucket: "ciclista-eletroni.appspot.com",
+  messagingSenderId: "537585784524",
+  appId: "1:537585784524:web:ab8e54fec65a4275c8d469"
 };
 
 firebase.initializeApp(firebaseConfig);
